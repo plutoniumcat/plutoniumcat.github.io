5407fc13dbd65c885f3a705070eb2af0aad65175
radiantbutterfly <alexandrialeemcpherson@gmail.com>
2023-05-07 04:46:52 +0900

    Toggle resume depending on language

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,59 +1,9 @@
-import resume from '../media/amcpherson_resume.pdf';
+import ENresume from '../data/amcpherson_resume.pdf';
+import JPresume from '../data/amcpherson_shokumu.pdf';
 import me2023 from '../media/me2023.jpg';
-
-const aboutEn = [
-    "Welcome to my website! I am a full-stack web developer with a passion for technology and a \
-    background in Japanese to English translation. I develop web applications using the MERN stack, Python, Flask, and SQL.",
-
-   "In addition to my technical abilities, I am fluent in Japanese and have worked \
-    as a professional translator in the past. This experience has given me a unique perspective \
-    on cross-cultural communication and has helped me to develop a keen attention to detail.",
-
-    "My interests extend beyond technology and language, as I am also an avid fan of music, art, \
-    and video games. I am fascinated by different cultures and languages, and I enjoy exploring \
-    the nuances that make each one unique.",
-
-    "In my free time, I like to experiment with creating visual effects and animations using code. \
-    I find it incredibly rewarding to be able to bring a creative vision to life through programming.",
-
-    "As a dedicated and passionate developer, I am always seeking new challenges and \
-    opportunities to learn and grow. I am excited to bring my skills and experiences to the \
-    table and collaborate with like-minded individuals and organizations. \
-    Thank you for visiting my website, and I look forward to connecting with you soon!"
-]
-
-const aboutJp = [
-    "私のウェブサイトへようこそ！私は、技術への情熱と日英翻訳のバックグラウンドを持つフルスタックのウェブ開発者です。\
-    MERNスタック、Python、Flask、SQLを使用してウェブアプリケーションを開発しています。",
-
-    "技術的な能力に加えて、私は日本語が流暢で、過去にプロの翻訳者として働いた経験があります。\
-    この経験により、異文化コミュニケーションに関するユニークな視点と、細部への鋭い注意を身につけることができました。",
-
-    "私の興味はテクノロジーや言語にとどまらず、音楽、アート、ビデオゲームの熱烈なファンでもあります。\
-    異なる文化や言語に魅了され、それぞれをユニークにするニュアンスを探求するのが好きです。",
-
-    "自由時間には、コードを使って視覚効果やアニメーションを作る実験が好きです。プログラミングを通じて、\
-    クリエイティブなビジョンに命を吹き込むことができることに、大きなやりがいを感じています。",
-
-    "熱心で情熱的な開発者として、私は常に新しい挑戦と学び、成長する機会を求めています。自分のスキルや経験を活かして、\
-    志を同じくする個人や組織とコラボレーションできることをとても嬉しく思っています。私のウェブサイトをご覧いただき、\
-    ありがとうございます！すぐにお会いできることを楽しみにしています！"
-]
-
-const timeline = [
-    {
-        "date": "October 2011",
-        "desc": "Graduated from Griffith University with a Bachelor of Arts in Japanese Language"
-    },
-    {
-        "date": "October 2022",
-        "desc": "Began Full-stack Web Development bootcamp at Coder Academy"
-    },
-    {
-        "date": "July 2023",
-        "desc": "End of Coder Academy bootcamp!"
-    }
-]
+import { aboutEn } from '../data/aboutdata';
+import { aboutJp } from '../data/aboutdata';
+import { timeline } from '../data/aboutdata';
 
 function About(props) {
     return (
@@ -77,7 +27,10 @@ function About(props) {
                 }
             </div>
             <div className="resume">
-                <a className="dl-resume" href={resume}>Resume (PDF)</a>
+                { props.langIsToggled ? 
+                    <a className="dl-resume" href={ JPresume }>職務経歴書 (PDF)</a> :
+                    <a className="dl-resume" href={ ENresume }>Resume (PDF)</a>
+                }
             </div>
         </section>
     )
